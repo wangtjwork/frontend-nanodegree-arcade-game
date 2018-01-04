184cc95a1577d96749ec72492016c01b3ca7f3da
Tianjian Wang <wangtjwork@gmail.com>
2018-01-04 12:21:28 -0600

    fill enemy class to basic

--- js/app.js
+++ js/app.js
@@ -1,25 +1,31 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy {
+  constructor: function(initialY, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.x = 0;
+    this.y = initialY;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
-};
+  }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+  // Update the enemy's position, required method for game
+  // Parameter: dt, a time delta between ticks
+  update: function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
-};
+    this.x += this.speed * dt;
+  }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+  // Draw the enemy on the screen, required method for game
+  render: function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+  }
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -35,7 +41,7 @@ Enemy.prototype.render = function() {
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
-    var allowedKeys = {
+    const allowedKeys = {
         37: 'left',
         38: 'up',
         39: 'right',
